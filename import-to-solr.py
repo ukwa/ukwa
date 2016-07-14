54c1a9735fd9f7200f8cc66b6c26cbb15f8e7173
Chris Peacock <chris@chrispeacock.name>
2016-07-14 11:49:00 +0100

    Fixed data types imported to Solr index (was adding arrays)

--- import-to-solr.py
+++ import-to-solr.py
@@ -1,32 +1,20 @@
-import rson
+import json
 import requests
 import configparser
 import codecs
 import solr
 
-# Notes
-# 
-# Collections key should not be quoted, i.e. "1" not "\"1\""
-# Collections could include the full collections properties
-# Targets do not report collection_cats
-# The /api/targets/bycollection/X hook appears to be fully recursive.
-# The instances are not available via the API (fix checked in)
-# Collection.updatedAt appears to be in milliseconds rather than the usual seconds since epoch.
-
 config = configparser.ConfigParser()
 config.read('act.cfg')
 
 actUrl = config.get('act', 'url')
+fullUrl = actUrl + "/login"
+print "fullUrl=" + fullUrl
 
 # create a connection to a Solr server
 solrUrl = config.get('solr', 'url')
 print "solrUrl=" + solrUrl
 s = solr.SolrConnection(solrUrl)
-
-fullUrl = actUrl + "/login"
-print "fullUrl=" + fullUrl
-
-#requests.encoding = 'utf8'
 
 response = requests.post(fullUrl, 
 	data={"email": config.get('act', 'username'), 
@@ -43,12 +31,12 @@ else:
 
 	all = requests.get(actUrl + "/api/collections", headers=headers)
 	
-	collections_tree = rson.loads(all.content)
+	collections_tree = json.loads(all.content)
 	for c in collections_tree:
 		c_id = int(c['key'])
 		col_url = actUrl + "/api/collections/%s" % c_id
 		col_req = requests.get(col_url, headers=headers)
-		col = rson.loads(col_req.content.decode('utf8').encode('ascii', 'ignore'))
+		col = json.loads(col_req.content.decode('utf8').encode('ascii', 'ignore'))
 		if col['field_publish'] == True:
 			print("Publishing...",c['title'])
 			
@@ -58,18 +46,19 @@ else:
 				name=col["name"],
 				description=col["description"]
 				)
+			s.commit()
 			
 			# Look up all Targets with in this Collection and add them.
 			t_url = actUrl + "/api/targets/bycollection/%s" % c_id
 			t_req = requests.get(t_url, headers=headers)
-			targets = rson.loads(t_req.content)			
+			targets = json.loads(t_req.content.decode('utf8').encode('ascii', 'ignore'))			
 
 			for t in targets:
 				target_url = actUrl + "/api/targets/%s" % int(t)
 				print "target_url=" + target_url
 				target_req = requests.get(target_url, headers=headers)
-				target = rson.loads(target_req.content.decode('utf8').encode('ascii', 'ignore'))
-				target['collection'] = c_id
+				target = json.loads(target_req.content.decode('utf8').encode('ascii', 'ignore'))
+				#target['collection'] = c_id
 															
 				# add a document to the Solr index
 				s.add(id=target["id"],
@@ -77,11 +66,12 @@ else:
 					parentId=c_id,
 					title=target["title"],
 					description=target["description"],
-					url=target["fieldUrls"][0].url,
+					url=target["fieldUrls"][0]["url"],
 					language=target["language"],
 					startDate=target["crawlStartDateISO"],
-					endDate=target["crawlEndDateISO"] 	
-					)				
+					endDate=target["crawlEndDateISO"]
+					)		
+				s.commit()
 
 		else:
 			print("Skipping...",c['title'])	
